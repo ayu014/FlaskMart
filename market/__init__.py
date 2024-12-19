ac3212ffef5468163dde505574b9ef0d7a251b8f
Ayush Ranjan <ayush0187cse@gmail.com>
2024-12-20 01:55:54 +0530

    Register Page

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '33d2c4b095678bab37664de5'
 db = SQLAlchemy(app)
 
 
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,9 @@
 from market import app
 from market.models import Item
 from flask import render_template
+from market.forms import RegisterForm
+
+
 
 
 @app.route("/")
@@ -12,3 +15,10 @@ def home():
 def market_page():
     items = Item.query.all()
     return render_template("market.html",items = items)
+
+@app.route("/register")
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form = form)
+
+
