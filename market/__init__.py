aebfd0ff96b2879a2ba276a3213b8869db03b8ce
Ayush Ranjan <ayush0187cse@gmail.com>
2024-12-20 21:37:22 +0530

    Hide info

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+load_dotenv()
+
+secret_key  = os.environ.get("SECRET_KEY")
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '33d2c4b095678bab37664de5'
+app.config['SECRET_KEY'] = secret_key
 db = SQLAlchemy(app)
 
 
