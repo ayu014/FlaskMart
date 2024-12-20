87c4718be04492d76f1f7e3e9e050734ef5bda30
Ayush Ranjan <ayush0187cse@gmail.com>
2024-12-20 23:19:21 +0530

    Add form validations

--- market/forms.py
+++ market/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Enter Username:')
-    email = StringField(label='Enter Email:')
-    password1 = PasswordField(label= 'Enter Password:')
-    password2 = PasswordField(label='Confirm Password:')
+    username = StringField(label='Enter Username:',validators=[Length(min=3,max=12), DataRequired()])
+    email = StringField(label='Enter Email:',validators=[Length(min=3,max=20), Email(), DataRequired()])
+    password1 = PasswordField(label= 'Enter Password:',validators=[Length(min=8,max=60), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:',validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
 
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from market.models import Item
-from flask import render_template
+from market.models import Item, User
+from flask import render_template, redirect,url_for
 from market.forms import RegisterForm, LoginForm
+from market import db
 
 
 
@@ -16,9 +17,19 @@ def market_page():
     items = Item.query.all()
     return render_template("market.html",items = items)
 
-@app.route("/register")
+@app.route("/register", methods = ['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username = form.username.data,
+                              email = form.email.data,
+                              password_hash = form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:                                   #If there are not any errors from the validations
+        for error_msg in form.errors.values():
+            print(f"There was an error with creating a user: {error_msg}")
     return render_template('register.html', form = form)
 
 @app.route("/login")
